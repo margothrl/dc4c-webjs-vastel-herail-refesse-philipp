bf5fd6a8c6781d28ace10c77d56870e1e167f735
Olivier Vastel <vastel.olivier@gmail.com>
2023-06-15 16:59:12 +0200

    Suppr Ligne inutile

--- front/javascript/index.js
+++ front/javascript/index.js
@@ -2,8 +2,8 @@ fetch ("http://127.0.0.1:3000/api/products")
     .then(Json => Json.json())
 
     function AllKanape (Json){
-        var nom = Json['name'];
+        
         for (var i=0; i<Json.length; i++){
             
         }
-    }
+    }  
