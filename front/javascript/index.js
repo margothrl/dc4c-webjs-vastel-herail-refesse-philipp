7b9bc94093d3565f7957024df3258f4ef37c5efb
Olivier Vastel <vastel.olivier@gmail.com>
2023-06-15 16:42:07 +0200

    JS Debut#

---
+++ front/javascript/index.js
@@ -0,0 +1,9 @@
+fetch ("http://127.0.0.1:3000/api/products")
+    .then(Json => Json.json())
+
+    function AllKanape (Json){
+        var nom = Json['name'];
+        for (var i=0; i<Json.length; i++){
+            
+        }
+    }
